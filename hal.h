ffdbcf69cd8eba8a3d919f9be8ff1a8519a3a7ce
Jari Ojanen <jojanen@elisanet.fi>
2019-01-29 22:11:26 +0200

    hal: one step closer to wiring interface (to utilize arduino drivers)

--- hal.h
+++ hal.h
@@ -3,35 +3,55 @@
 #include "hal_pins.h"
 #include "buffer.h"
 
-#define IO_MODE_INPUT  0
-#define IO_MODE_OUTPUT 1
+// Constants (wiring compatible see http://wiring.org.co/reference)
+//
+#define _INPUT  0
+#define _OUTPUT 1
 
 #define TRUE 1
 #define FALSE 0
 
+#define HIGH 1
+#define LOW  0
+
+
 extern void delay_us(uint16_t time);
 
+// module input output
+//
 extern void io_init();
-extern void io_pin_init(pin_t pin, uint8_t mode);
-extern void io_mode(pin_t, uint8_t mode);
-extern void io_set(pin_t pin);
-extern void io_clear(pin_t pin);
-extern uint8_t io_read(pin_t pin);
+extern void pinMode(pin_t pin, uint8_t mode);
+extern void digitalWrite(pin_t pin, uint8_t value);
+extern uint8_t digitalRead(pin_t pin);
 
+class SerialBase
+{
+public:
+    void begin(uint16_t rate);
+    uint8_t read( );
+    void write(uint8_t ch);
+    void print(uint8_t val);
+    void println();
+    uint8_t available();
+    uint8_t peek();
+    void flush();
+    void end();
+};
+
+// module serial port
+//
 extern void uart_init(uint8_t port);
 extern void uart_print(uint8_t port, buffer_t *buf);  // async send command
 extern void uart_sync(uint8_t port, buffer_t *buf);
-
 extern void uart_send(uint8_t port, char ch);    // old version use uart_print
 extern void uart_sends(uint8_t port, char *buf); //
 extern void uart_send_nl(uint8_t port);
 
-//extern void uart_nl(uint8_t port);                    // in hal_common
-//extern void uart_hex8(uint8_t port, uint8_t value);   // in hal_common
-//extern void uart_hex16(uint8_t port, uint16_t value); // in hal_common
-//extern void uart_dec8(uint8_t port, uint8_t value);   // in hal_common
-
+// module timer
+//
 extern void timer2_init();
 
+// module SPI serial bus
+//
 extern void spi_init(uint8_t port);
 extern uint16_t spi_write(uint8_t port, uint16_t data);
