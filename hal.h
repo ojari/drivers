761fc3b13cca2ca9907e2b01d24f34caeb5ce6ed
Jari Ojanen <jojanen@elisanet.fi>
2019-05-03 22:30:31 +0300

    hal: added SpiBase and some virtual functions into SerialBase

--- hal.h
+++ hal.h
@@ -24,22 +24,24 @@ extern void pinMode(pin_t pin, uint8_t mode);
 extern void digitalWrite(pin_t pin, uint8_t value);
 extern uint8_t digitalRead(pin_t pin);
 
+// module serial port
+//
 class SerialBase
 {
 public:
-    void begin(uint16_t rate);
-    uint8_t read( );
-    void write(uint8_t ch);
+    virtual void begin(uint16_t rate) = 0;
+    virtual void write(uint8_t ch) = 0;
+    virtual void end() = 0;
+
+    uint8_t read();
     void print(uint8_t val);
     void println();
+    void println(const char* str);
     uint8_t available();
     uint8_t peek();
     void flush();
-    void end();
 };
 
-// module serial port
-//
 extern void uart_init(uint8_t port);
 extern void uart_print(uint8_t port, buffer_t *buf);  // async send command
 extern void uart_sync(uint8_t port, buffer_t *buf);
@@ -51,7 +53,45 @@ extern void uart_send_nl(uint8_t port);
 //
 extern void timer2_init();
 
+
+// class for SPI bus configuration parameters
+//
+enum class SPIMode {
+    SPI_MODE0 = 0,
+    SPI_MODE1 = 1,
+    SPI_MODE2 = 2,
+    SPI_MODE3 = 4
+};
+
+enum class SPIBitOrder {
+    MSBFIRST,
+    LSBFIRST
+};
+
+class SPISettings
+{
+public:
+    SPISettings(uint32_t clk, SPIBitOrder bo, SPIMode m) :
+	clock{clk},
+	bitOrder{bo},
+	mode{m}
+    {
+    }
+    friend class SpiBase;
+protected:
+    uint32_t clock;
+    SPIBitOrder bitOrder;
+    SPIMode mode;
+};
+
+
 // module SPI serial bus
 //
-extern void spi_init(uint8_t port);
-extern uint16_t spi_write(uint8_t port, uint16_t data);
+class SpiBase
+{
+public:    
+    virtual void beginTransaction(const SPISettings set) = 0;
+    virtual uint8_t transfer(uint8_t byte) = 0;
+    virtual uint16_t transfer16(uint16_t word) = 0;
+    virtual void endTransaction() = 0;
+};
