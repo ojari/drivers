e104ffc45d5dba16bd1471b21dd6283bb2ba81cc
Jari <jojanen@elisanet.fi>
2024-01-08 14:48:40 +0200

    update win/Arduino.h

--- win/Arduino.h
+++ win/Arduino.h
@@ -8,14 +8,28 @@
  */
 typedef enum errors {
     ERR_MY     = 0x01,
+    ERR_SPI_OPEN,
+    ERR_SPI_SET_MODE,
+    ERR_SPI_SET_BITS,
+    ERR_SPI_SET_SPEED,
+    ERR_SPI_TRANSFER,
+    ERR_GPIO_INIT,
+    ERR_GPIO_GETLINE,
+    ERR_GPIO_MODE_OUT,
+    ERR_GPIO_MODE_IN,
+    ERR_GPIO_INVALID_MODE
 } errorcode_t;
 
 extern void _error(errorcode_t code);
-
+extern void digitalInit();
 
 class PcSPI {
 public:
+    void beginTransaction(const SPISettings& set);
     uint8_t transfer(uint8_t data);
+    void endTransaction();
+private:
+    int fd;
 };
 
 extern PcSPI SPI;
--- win/hal_pins.h
+++ win/hal_pins.h
@@ -1,7 +1,20 @@
 #pragma once
 
 typedef enum pin {
-    PIN_NRF24_CE  = 0,
-    PIN_NRF24_CSN,
-    TEST
+    P3_SDA = 0,
+    P5_SCL,
+    P7,
+    P11,
+    P13,
+    P15,
+    P19_SPI_MOSI,
+    P21_SPI_MISO,
+    P23_SPI_SCLK,
+    P12,
+    P16,
+    P18,
+    P22,
+    P24_SPI_CS0,
+    P26_SPI_CS1,
+    PIN_MAX
 } pin_t;
